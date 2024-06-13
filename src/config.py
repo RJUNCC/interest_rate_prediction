e35e6a0bbeeb6ed7a2c4914c9fbf7c9c82ab4706
RJUNCC <20bigal04@ryeguytech.com>
2024-06-13 14:22:07 -0400

    added files

---
+++ src/config.py
@@ -0,0 +1,7 @@
+EXCEL_DATA_PATH   = "../data/xlsx/"
+CPI_PATH          = EXCEL_DATA_PATH + "cpi/"
+GDP_PATH          = EXCEL_DATA_PATH + "gdp/"
+PPI_PATH          = EXCEL_DATA_PATH + "ppi/"
+UNEMPLOYMENT_PATH = EXCEL_DATA_PATH + "unemployment/"
+RETAIL_SALES_PATH = EXCEL_DATA_PATH + "retail_sales/"
+FED_FUNDS_PATH    = EXCEL_DATA_PATH + "fed_funds_rate/"
